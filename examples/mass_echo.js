f2feea299c9a726bfc0f28ce698cbfc3c470008a
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-05 13:27:02 -0400

    Add mass echo example

---
+++ examples/mass_echo.js
@@ -0,0 +1,17 @@
+/*
+ * This is echo.js but with mineflayer-swarm.
+ *
+ * Instead of one bot echoing everyone else,
+ * every bot in the swarm echoes other players.
+ */
+const mineflayerSwarm = require('mineflayer-swarm')
+
+const swarm = await mineflayerSwarm.createSwarm(require('./auth.json'), {
+  host: 'localhost',
+  port: 25565
+})
+
+swarm.on('chat', (bot, username, message) => {
+  if (swarm.isSwarmMember(username)) return
+  bot.chat(message)
+})
