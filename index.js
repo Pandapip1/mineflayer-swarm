adafd3da45a9b7fff7a27cc2545fe01c30cbd0f2
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-11-05 13:00:50 -0400

    Fix bugs, reorganize, and lint

--- index.js
+++ index.js
@@ -8,33 +8,29 @@ const mineflayer = require('mineflayer')
 const { EventEmitter } = require('events')
 const assert = require('assert')
 
-async function createSwarm(auths, options = {}){
-  var swarm = new EventEmitter()
+async function createSwarm (auths, options = {}) {
+  const swarm = new EventEmitter()
   // properties
   swarm.bots = []
   swarm.plugins = []
   // methods
-  swarm.addSwarmMember = async function(auth) {
-    let bot = mineflayer.createBot({...options, ...auth})
-    bot.my_opts = {...options, ...auth}
-    bot.onAny((event, value) => swarm.emit(event, { bot, value }))
-    bot.once('inject_allowed', () => swarm.plugins.forEach((plugin) => {
-      plugin(bot, options)
-    }))
-    bot.once('inject_allowed', () => bot.inject_allowed = true)
+  swarm.addSwarmMember = async function (auth) {
+    const bot = mineflayer.createBot({ ...options, ...auth })
+    bot.my_opts = { ...options, ...auth }
+    bot.onAny(swarm.emit) // nice shortcut
     swarm.bots.push(bot)
   }
-  swarm.isSwarmMember = function(username) {
-    return swarm.bots.any(bot => bot.username == username)
+  swarm.isSwarmMember = function (username) {
+    return swarm.bots.any(bot => bot.username === username)
   }
-  swarm.execAll = async function(fun) {
+  swarm.execAll = async function (fun) {
     return await Promise.all(swarm.bots.map(async bot => {
       let res = fun(bot)
       if (res instanceof Promise) res = await res
       return res
     }))
   }
-  swarm.loadPlugin = function(plugin) {
+  swarm.loadPlugin = function (plugin) {
     assert.ok(typeof plugin === 'function', 'plugin needs to be a function')
 
     if (swarm.hasPlugin(plugin)) {
@@ -47,21 +43,28 @@ async function createSwarm(auths, options = {}){
       if (bot.inject_allowed) plugin(bot, bot.my_opts)
     })
   }
-  swarm.loadPlugins = function(plugins) {
+  swarm.loadPlugins = function (plugins) {
     assert.ok(plugins.every(plugin => typeof plugin === 'function'), 'plugins need to be an array of functions')
 
     plugins.forEach((plugin) => {
       swarm.loadPlugin(plugin)
     })
   }
-  swarm.hasPlugin = function(plugin) {
+  swarm.hasPlugin = function (plugin) {
     return swarm.plugins.indexOf(plugin) >= 0
   }
   // init swarm
   auths.forEach(swarm.addSwarmMember)
-  
-  swarm.on('end', (bot, _ignored) => {
-    swarm.bots = swarm.bots.filter(x => bot.username != x.username)
+  // remove disconnected members
+  swarm.on('end', bot => {
+    swarm.bots = swarm.bots.filter(x => bot.username !== x.username)
+  })
+  // plugin injection
+  swarm.on('inject_allowed', bot => {
+    bot.inject_allowed = true
+    swarm.plugins.forEach((plugin) => {
+      plugin(bot, options)
+    })
   })
   
   return swarm
