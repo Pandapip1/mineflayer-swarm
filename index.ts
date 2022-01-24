51d34f3149eb374896c826b6dbb2772105fffea0
Pandapip1 <gavinnjohn@gmail.com>
2022-01-24 18:45:18 +0000

    Convert to Typescript

---
+++ index.ts
@@ -0,0 +1,103 @@
+if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 14) {
+  console.error('Your node version is currently', process.versions.node)
+  console.error('Please update it to a version >= 14.x.x from https://nodejs.org/')
+  process.exit(1)
+}
+
+import { createBot, Bot } from "mineflayer"
+import { ClientOptions } from "minecraft-protocol"
+import EventEmitter from "eventemitter3"
+import assert from "assert"
+
+type Plugin = (((bot : Bot) => any) | ((bot : Bot, opts : ClientOptions) => any))
+
+export class Swarm extends EventEmitter {
+  bots: Bot[]
+  plugins: Plugin[]
+  options: Partial<ClientOptions>
+
+  constructor(options: Partial<ClientOptions>) {
+    super()
+    this.bots = []
+    this.plugins = []
+    this.options = options
+
+    this.on('error', (bot, ...errors) => console.error(...errors))
+
+    // remove disconnected members
+    this.on('end', bot => {
+      this.bots = this.bots.filter(x => bot.username !== x.username)
+    })
+
+    // plugin injection
+    this.on('inject_allowed', bot => {
+      (bot as any).swarm_options.inject_allowed = true
+      this.plugins.forEach((plugin) => {
+        plugin(bot, (bot as any).swarm_options.bot_options)
+      })
+    })
+  }
+
+  addSwarmMember(auth : Partial<ClientOptions>) : void {
+    // fix for microsoft auth
+    if (auth.auth === 'microsoft') auth.authTitle = '00000000402b5328'
+    // create bot and save its options
+    const bot = createBot({ ...this.options, ...auth } as ClientOptions) as Bot // Not sure why cast is neccesary...
+    // ugly but neccesary
+    (bot as any).swarm_options = {} as any
+    (bot as any).swarm_options.bot_options = { ...this.options, ...auth } as any
+    (bot as any).swarm_options.inject_alowed = false as any
+    // monkey patch bot.emit
+    var oldEmit = bot.emit
+    bot.emit = (event, ...args) => {
+      this.emit(event, this, ...args)
+      return oldEmit(event, ...args)
+    }
+    // add bot to swarm
+    this.bots.push(bot)
+  }
+
+  isSwarmMember(username : string) : boolean {
+    return this.bots.some(bot => bot.username === username)
+  }
+
+  async execAll<Type>(fun : ((bot : Bot) => Type | Promise<Type>)) : Promise<Type[]> {
+    return await Promise.all(this.bots.map(async bot => {
+      let res = fun(bot)
+      if (res instanceof Promise) res = await res
+      return res
+    }))
+  }
+
+  loadPlugin(plugin : Plugin) {
+    assert.ok(typeof plugin === 'function', 'plugin needs to be a function')
+
+    if (this.hasPlugin(plugin)) {
+      return
+    }
+
+    this.plugins.push(plugin)
+
+    this.bots.forEach(bot => {
+      if ((bot as any).swarm_options.inject_allowed) plugin(bot, (bot as any).swarm_options.bot_options)
+    })
+  }
+
+  loadPlugins(plugins : Plugin[]) : void {
+    plugins.forEach(this.loadPlugin)
+  }
+
+  hasPlugin(plugin : Plugin) : boolean {
+    return this.plugins.indexOf(plugin) >= 0
+  }
+}
+
+export function createSwarm (auths : Partial<ClientOptions>[], options : Partial<ClientOptions> = {}) {
+  // create swarm object
+  const swarm = new Swarm(options)
+
+  // init swarm
+  auths.forEach(swarm.addSwarmMember)
+
+  return swarm
+}
