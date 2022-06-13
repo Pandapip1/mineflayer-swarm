a3acd3aa4c7dd9028bd640262fcc08428d946978
Pandapip1 <gavinnjohn@gmail.com>
2022-06-13 16:36:43 +0000

    Revert "Implementation of module API done"
    This reverts commit 7adc1b7762275635706fa7dabb28ec4922a81688.

--- index.ts
+++ index.ts
@@ -1,6 +1,7 @@
+import { createBot, Bot } from 'mineflayer';
 import { ClientOptions } from 'minecraft-protocol';
-import { spawn, Thread, Worker, ModuleThread } from 'threads';
-import { BotWorker } from './worker';
+import EventEmitter from 'eventemitter3';
+import assert from 'assert';
 
 if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[0]) < 16) {
   console.error('Your node version is currently', process.versions.node);
@@ -8,36 +9,98 @@ if (typeof process !== 'undefined' && parseInt(process.versions.node.split('.')[
   process.exit(1);
 }
 
-export default class Swarm {
-  workers: ModuleThread<BotWorker>[];
-  plugins: string[];
+export type Plugin = (((bot: Bot) => null) | ((bot: Bot, opts: ClientOptions) => null));
+
+export class BotSwarmData {
+  botOptions?: ClientOptions;
+  injectAllowed = false;
+}
+
+export interface ISwarm {
+    say(): string;
+};
+
+interface SwarmBot extends Bot {
+  swarmOptions?: BotSwarmData
+}
+
+export class Swarm extends EventEmitter {
+  bots: SwarmBot[];
+  plugins: Plugin[];
   options: Partial<ClientOptions>;
 
   constructor (options: Partial<ClientOptions>) {
-    this.workers = [];
+    super();
+    this.bots = [];
     this.plugins = [];
     this.options = options;
-  }
 
-  async addSwarmMember (auth: Partial<ClientOptions>) {
-    // Fix for MS Auth
-    if (auth.auth === 'microsoft') auth.authTitle = '00000000402b5328';
+    this.on('error', (bot, ...errors) => console.error(...errors));
 
-    // Create Bot
-    const worker = await spawn<BotWorker>(new Worker("./worker"));
-    const endObservable = worker.initialize({ ...this.options, ...auth } as ClientOptions);
-    this.workers.push(worker);
-    
-    // Load Plugins
-    await Promise.all(this.plugins.map(worker.loadPlugin));
+    // remove disconnected members
+    this.on('end', bot => {
+      this.bots = this.bots.filter(x => bot.username !== x.username);
+    });
 
-    // Remove on DC
-    endObservable.subscribe(() => {
-      Thread.terminate(worker);
+    // plugin injection
+    this.on('inject_allowed', bot => {
+      bot.swarmOptions.injectAllowed = true;
+      this.plugins.forEach((plugin) => {
+        plugin(bot, bot.swarmOptions.botOptions);
+      });
     });
   }
 
-  async loadPlugin (plugin: string) {
-    this.plugins.push(plugin);
+  addSwarmMember (auth: Partial<ClientOptions>): void {
+    // fix for microsoft auth
+    if (auth.auth === 'microsoft') auth.authTitle = '00000000402b5328';
+    // create bot and save its options
+    const botOptions: Partial<ClientOptions> = { ...this.options, ...auth };
+    const bot: SwarmBot = createBot(botOptions as ClientOptions);
+    bot.swarmOptions = new BotSwarmData();
+    bot.swarmOptions.botOptions = botOptions as ClientOptions;
+    // monkey patch bot.emit
+    const oldEmit = bot.emit;
+    bot.emit = (event, ...args) => {
+      this.emit(event, this, ...args);
+      return oldEmit(event, ...args);
+    };
+    // add bot to swarm
+    this.bots.push(bot);
   }
+
+  isSwarmMember (username: string): boolean {
+    return this.bots.some(bot => bot.username === username);
+  }
+
+  loadPlugin (plugin: Plugin): void {
+    assert.ok(typeof plugin === 'function', 'plugin needs to be a function');
+
+    if (this.hasPlugin(plugin)) {
+      return;
+    }
+
+    this.plugins.push(plugin);
+
+    this.bots.forEach(bot => {
+      if (bot.swarmOptions?.botOptions !== undefined && bot.swarmOptions?.injectAllowed) {
+        plugin(bot, bot.swarmOptions.botOptions);
+      }
+    });
+  }
+
+  hasPlugin (plugin: String): boolean {
+    import 
+    return this.plugins.includes(plugin);
+  }
+}
+
+export function createSwarm (auths: Array<Partial<ClientOptions>>, options: Partial<ClientOptions> = {}): Swarm {
+  // create swarm object
+  const swarm = new Swarm(options);
+
+  // init swarm
+  auths.forEach(swarm.addSwarmMember);
+
+  return swarm;
 }
--- worker.ts
+++ worker.ts
@@ -1,78 +1,10 @@
 import { createBot, Bot } from 'mineflayer';
 import { ClientOptions } from 'minecraft-protocol';
+import EventEmitter from 'eventemitter3';
 import assert from 'assert';
-import { expose } from 'threads/worker';
-import { Subject } from 'observable-fns';
 
-type Plugin = (((bot: Bot) => null) | ((bot: Bot, opts: ClientOptions) => null));
+export class SwarmMember {
+  
+};
 
-let hasInitialized : boolean = false;
-let bot : Bot;
-
-let hasPluginAbility : boolean = false;
-let allPlugins : Plugin[] = [];
-
-let doRelog = true;
-let exponentialBackoff : number = 1;
-let backoffTimeout : NodeJS.Timeout;
-let relogTimeout : NodeJS.Timeout;
-
-const swarmBot = {
-    initialize(options : ClientOptions) : Subject<boolean> {
-        assert(!hasInitialized, 'Bot already initialized.');
-
-        const subject = new Subject<boolean>();
-
-        bot = createBot(options);
-        hasInitialized = true;
-
-        let ogEnd : (reason?: string | undefined) => void = bot.end;
-        bot.end = (reason?: string | undefined) => {
-            doRelog = false;
-            ogEnd(reason);
-        };
-
-        bot.on('inject_allowed', () => {
-            hasPluginAbility = true;
-            allPlugins.forEach(bot.loadPlugin);
-        });
-
-        bot.once('end', () => {
-            if (doRelog) {
-                if (backoffTimeout){
-                    clearTimeout(backoffTimeout);
-                }
-
-                hasInitialized = false;
-                hasPluginAbility = false;
-
-                relogTimeout = setTimeout(() => {
-                    swarmBot.initialize(options);
-
-                    backoffTimeout = setTimeout(() => {
-                        exponentialBackoff = 1;
-                    }, 5000);
-                }, exponentialBackoff * 1000);
-
-                exponentialBackoff *= 2;
-            } else {
-                subject.next(false);
-                subject.complete();
-            }
-        });
-
-        return subject;
-    },
-    loadPlugin(plugin: string) {
-        let loadedPlugin : Plugin = require(plugin);
-        allPlugins.push(loadedPlugin);
-
-        if (hasPluginAbility) {
-            bot.loadPlugin(loadedPlugin);
-        }
-    }
-}
-
-export type BotWorker = typeof swarmBot;
-
-expose(swarmBot);
+export default new SwarmMember();
